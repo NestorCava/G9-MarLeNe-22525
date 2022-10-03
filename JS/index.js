f7a4dcd97ab4010da439b4bc5a9fea7a82a63d95
leoschy <schymansky.leonardo@gmail.com>
2022-10-03 19:30:00 -0300

    intento de agregar navbar y secciones

---
+++ JS/index.js
@@ -0,0 +1,22 @@
+let idNavbar = `
+    <nav id="nav-bar">
+    <ol>
+      <li class="nav-link">
+        <a href="./home.html">Home</a>
+      </li>
+      
+      <li class="nav-link">
+        <a href="./nosotros.html">Nosotros</a>
+      </li>
+      
+      <li class="nav-link">
+        <a href="./indicaciones.html">Indicaciones</a>        
+      </li>
+
+      <li class="nav-link">
+        <a href="./contacto">Contacto</a>        
+      </li>
+    </ol>
+  </nav>
+`
+document.getElementById("idnavbar").innerHTML=idNavbar;
